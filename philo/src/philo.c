186fe424b7564eb0955366b7fd69ce26404f120a
etienne-hd <hode.etienne@gmail.com>
2025-11-15 11:04:58 +0100

    Changed: static usleep are now dynamic usleep (5 * number of philosopher)

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,7 +6,7 @@
 /*   By: ehode <ehode@student.42angouleme.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/11 09:59:24 by ehode             #+#    #+#             */
-/*   Updated: 2025/11/14 18:56:45 by ehode            ###   ########.fr       */
+/*   Updated: 2025/11/15 10:58:14 by ehode            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,7 @@ static int	philo_lock_fork(t_philo *philo, t_fork *fork)
 			break ;
 		}
 		pthread_mutex_unlock(&fork->lock);
-		usleep(1000);
+		usleep(5 * philo->sim->number_of_philo);
 	}
 	pthread_mutex_unlock(&fork->lock);
 	philo_display(philo, "has taken a fork", 0);
@@ -123,7 +123,8 @@ void	*philo_start(void *arg)
 		}
 		else if (get_timestamp() >= philo->next_state_in)
 			next_state(philo);
-		usleep(1000);
+		else
+			usleep(5 * philo->sim->number_of_philo);
 	}
 	return (NULL);
 }
